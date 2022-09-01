c62ed5100c0af46808c8f67cf47cfcc72d01045e
Charan <sricharan.k@swiggy.in>
2022-09-01 22:15:00 +0530

    commit

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,28 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import FollowupAction
+from rasa_sdk.executor import CollectingDispatcher
+
+def formulate_message(*msg_parts):
+    message = ""
+    for msg_part in msg_parts:
+        message += msg_part + "\n"
+    return message
+
+class ActionShowItems(Action):
+
+    def name(self) -> Text:
+        return "action_show_items"
+
+    def run(self, dispatcher: CollectingDispatcher, tracker: Tracker, domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        message = formulate_message("In your area we have the following available on instamart", "Your order will be delivered in 30-40min, rest assured - order away!")
+        buttons = []
+        buttons.append({"title": "vegetables", "payload": "/buy_vegetables"})
+        buttons.append({"title": "fruits", "payload": "/buy_fruits"})
+        buttons.append({"title": "milk & bread", "payload": "/buy_milk"})
+        dispatcher.utter_message(text=message, buttons=buttons)
+        return [FollowupAction("action_listen")]
